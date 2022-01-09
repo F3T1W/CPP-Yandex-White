84a518f15ee99a823970f6caabad61bce0a255bb
F3T1W <F3T1W@WIN-P0PM2B58OND>
2022-01-09 03:29:51 +0300

    Добавьте файлы проекта.

---
+++ App.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Whore
+{
+	string name;
+	int age;
+	bool virgin;
+};
+
+int main()
+{
+	/*
+	cout << "Hello World! ";
+	vector<string> strings = {"Katya", "Modnyi", "Strij"};
+	for (int i = 0; i < strings.size(); i++)
+	{
+		cout << strings[i] << " ";
+	}
+	map<int, string> pussy;
+	pussy[1] = "Jopa";
+	pussy[2] = "Pysya";
+	cout << endl << "Ento moya " << pussy[1];
+	cout << strings.size();
+	vector<Whore> eblya;
+	eblya.push_back({ "Katya", 20, true });
+	eblya.push_back({ "Ee Mat'", 100000, false });
+	cout << eblya[0].name << " " << eblya[0].age << " " << eblya[0].virgin;
+	*/
+	/*
+	vector<int> jija = {3, 2, 1, 4, 4};
+	int quantity = count(begin(jija), end(jija), 4);
+	sort(begin(jija), end(jija));
+	for (auto c : jija)
+	{
+		cout << c << ",";
+	}
+	cout << quantity;
+	*/
+	return 0;
+}
