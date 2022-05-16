ac54f5099e6a1215a277e9bea511023f60a0dd96
F3T1W <particular0010abyss@gmail.com>
2022-05-16 23:12:52 +0300

    Done finding min string and square equalation

--- App.cpp
+++ App.cpp
@@ -1,7 +1,49 @@
-#include "Equalation.h"
-#include <vector>
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+static std::string Calculate(float, float, float);
 
 int main(void)
 {
-	std::string x = Equalation::Calculate(0, 4, 10);
+	std::string x = Calculate(0, 4, 10);
+}
+
+static std::string Calculate(float a, float b, float c)
+{
+	if (a != 0)
+	{
+		double y = (-b + std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a);
+		double z = (-b - std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a);
+
+		if (isnan(y) || isnan(z))
+		{
+			std::cout << "";
+			return "";
+		}
+		if (z != y)
+		{
+			std::cout << std::setprecision(2) << y << " " << std::setprecision(2) << z;
+			return std::to_string(y) + " " + std::to_string(z);
+		}
+		else
+		{
+			std::cout << std::setprecision(2) << z;
+			return std::to_string(z);
+		}
+	}
+	else
+	{
+		double notNullB = -c / b;
+		if (b != 0)
+		{
+			std::cout << std::setprecision(2) << notNullB;
+			return std::to_string(notNullB);
+		}
+		else
+		{
+			std::cout << "";
+			return "";
+		}
+	}
 }
--- Equalation.h
+++ Equalation.h
@@ -12,9 +12,8 @@ public:
 	{
 		if (a != 0)
 		{
-			double y, z;
-			y = (-b + std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a);
-			z = (-b - std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a);
+			double y = (-b + std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a);
+			double z = (-b - std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a);
 			
 			if (isnan(y) || isnan(z))
 			{
@@ -23,23 +22,22 @@ public:
 			}
 			if (z != y)
 			{
-				std::string x = std::to_string((-b + std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a)) + " " + std::to_string((-b - std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a));
-				std::cout << ((-b + std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a)) << " " << ((-b - std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a));
-				return x; // TODO сократить икс и возврат и сделать правильный возврат
+				std::cout << y << " " << z;
+				return std::to_string(y) + " " + std::to_string(z);
 			}
 			else
 			{
-				std::string x = std::to_string((-b - std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a));
-				std::cout << ((-b - std::sqrt(std::pow(b, 2) - (4 * a * c))) / (2 * a));
-				return x;
+				std::cout << z;
+				return std::to_string(z);
 			}
 		}
 		else
 		{
+			double notNullB = -c / b;
 			if (b != 0)
 			{
-				std::cout << std::to_string(-c / b);
-				return std::to_string(- c / b);
+				std::cout << std::to_string(notNullB);
+				return std::to_string(notNullB);
 			}
 			else
 			{
