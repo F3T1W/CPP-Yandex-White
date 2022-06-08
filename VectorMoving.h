b31610718bcad9e03d52843a01032308c6b998c5
Владислав Матюшин <particular0010abyss@gmail.com>
2022-06-08 10:27:53 +0300

    const auto& added

--- VectorMoving.h
+++ VectorMoving.h
@@ -9,7 +9,7 @@ public:
 	static void Move(vector<string>& source, vector<string>& destination)
 	{
 		destination.reserve(source.size( )); // Allocating minimal needed memory and not creating new if not necessary
-		for (auto x : source)
+		for (const auto& x : source)
 		{
 			destination.push_back(x);
 		}
