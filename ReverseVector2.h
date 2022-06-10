b7a6f8c6b18750dbe3db6f410bed532270734c6e
Владислав Матюшин <particular0010abyss@gmail.com>
2022-06-10 21:51:38 +0300

    Fixed issue

--- ReverseVector2.h
+++ ReverseVector2.h
@@ -8,15 +8,10 @@ using namespace std;
 class ReverseVector2
 {
 public:
-	static vector<int> Reversed(const vector<int>& v)
+	static vector<int> Reversed(vector<int> v)
 	{
-		vector<int> returning;
-		for (auto x : v)
-		{
-			returning.push_back(x);
-		}
-		reverse(begin(returning), end(returning));
-		return returning;
+		reverse(begin(v), end(v));
+		return v;
 	}
 };
 
