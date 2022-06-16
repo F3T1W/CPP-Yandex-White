e1d6d6bddeeb05049fb45646638bb4d5838739c0
Владислав Матюшин <particular0010abyss@gmail.com>
2022-06-16 12:37:29 +0300

    Micro upd

--- Test2/test.cpp
+++ Test2/test.cpp
@@ -19,6 +19,16 @@ TEST(SORTABS, SORTABS0)
 	istringstream iss("2 -4 3");
 	ostringstream oss;
 	SortAbs::DoIt(iss, oss);
+	EXPECT_EQ(oss.str(), "3 -4 ");
+}
+
+TEST(SORTABS, SORTABS1)
+{
+	istringstream iss("3 1 -3 2");
+	ostringstream oss;
+	SortAbs::DoIt(iss, oss);
+	EXPECT_EQ(oss.str(), "1 2 -3 ");
+	
 }
 */
 #pragma endregion
@@ -30,6 +40,15 @@ TEST(SORTLOWER, SORTLOWER0)
 	istringstream iss("2 qq Aa");
 	ostringstream oss;
 	SortLowerCase::DoIt(iss, oss);
+	EXPECT_EQ(oss.str(), "Aa qq ");
+}
+
+TEST(SORTLOWER, SORTLOWER1)
+{
+	istringstream iss("3 aA Cc bb");
+	ostringstream oss;
+	SortLowerCase::DoIt(iss, oss);
+	EXPECT_EQ(oss.str(), "aA bb Cc ");
 }
 */
 #pragma endregion
--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-using namespace std;
-
 int main()
-{   
+{
     return 0;
 }
